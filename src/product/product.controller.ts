7fe27a63818fd05d24e1ac11bf456abe6692d88a
정훈박 <fireking5997@gmail.com>
2022-05-24 21:37:19 +0900

    add:findone product

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,6 +1,6 @@
 import { RegisterProductDto } from './dtos/product.register.dto';
 import { ProductService } from './product.service';
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller()
 export class ProductController {
@@ -15,5 +15,10 @@ export class ProductController {
   async register(@Body() body: RegisterProductDto) {
     const product = await this.productService.save(body);
     return this.productService.findOne({ id: product['id'] });
+  }
+
+  @Get('admin/product/:id')
+  async one(@Param() id: number) {
+    return this.productService.findOne({ id });
   }
 }
